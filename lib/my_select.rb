37dae92b160d17db71cc1cd0e06279aedf830b80
Victoria Rathsmill <vlrathsmill@gmail.com>
2017-01-12 23:21:23 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+ i = 0
+ result = []
+
+ while i < array.length
+  if yield(array[i]) == true
+    result.push(array[i])
+  end
+   i += 1
+ end
+result
 end
